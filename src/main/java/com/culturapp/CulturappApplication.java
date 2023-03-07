ca32fb4989b295f1d502725b7d40198ebe7d4833
Julian Cardozo <julian@pambumedia.com>
2023-03-07 11:52:06 -0300

    https://start.spring.io/ create project

---
+++ src/main/java/com/culturapp/CulturappApplication.java
@@ -0,0 +1,13 @@
+package com.culturapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturappApplication.class, args);
+	}
+
+}
