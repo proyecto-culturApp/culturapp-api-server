99de1d5f8421211c9b7dc3dcae7eab17f2f25770
Julian Cardozo <julian@pambumedia.com>
2023-03-09 09:30:10 -0300

    Create DummyController.java

---
+++ src/main/java/com/culturapp/controllers/DummyController.java
@@ -0,0 +1,13 @@
+package com.culturapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DummyController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hola, este es un microservicio dummy!";
+    }
+}
